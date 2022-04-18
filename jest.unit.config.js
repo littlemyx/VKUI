2dfbe66a0cd5a482dcdeeeaa45d6905bd5e79b08
Daniil Suvorov <severecloud@gmail.com>
2022-04-18 14:29:30 +0300

    feat: add postcss-restructure-variable

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
   displayName: "unit",
-  roots: [path.join(__dirname, "src")],
+  roots: [path.join(__dirname, "src"), path.join(__dirname, "tasks")],
   setupFilesAfterEnv: [path.join(__dirname, "src/testing/setup.ts")],
   collectCoverage: true,
   collectCoverageFrom: ["src/*/**/*.{ts,tsx}"],
